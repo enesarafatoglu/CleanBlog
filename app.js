9e4a944735975975dda3cab54fcf7a4088d154b4
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-17 01:31:39 +0300

    Template engine added

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
 
 const port = 3000;
 
