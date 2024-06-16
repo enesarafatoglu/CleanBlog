3d43f2c94bfc4eaf0eb68f9d1d177970aba2b348
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-16 22:36:00 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//MIDDLEWARES
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog Title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
 });
 
 const port = 3000;
