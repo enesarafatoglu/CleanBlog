0b644ad2e03104b260fdbaf249daf1056215129e
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-19 16:40:42 +0300

    mongoose dahil edildi

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Blog = require('./models/Blog');
 
 const app = express();
+
+mongoose
+  .connect('mongodb://localhost/blog-test-db')
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+  });
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.render('index', {
+    blogs
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -22,8 +38,9 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.get('/post', (req, res) => {
-  res.render('post');
+app.post('/blogs', async (req, res) => {
+  await Blog.create(req.body)
+  res.redirect('/');
 });
 
 
