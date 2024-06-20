4863b8ebedf39987c4ab5cf18d09ab5eb13719f4
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-20 20:34:37 +0300

    finished project

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
-const path = require('path');
-const Blog = require('./models/Blog');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const ejs = require("ejs");
+const Post = require("./models/Post");
+const methodOverride = require("method-override");
 
 const app = express();
+
+const {
+  getAllPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost
+} = require("./controllers/postController");
+const {
+  aboutPage,
+  homePage,
+  addPage,
+  editPage
+} = require("./controllers/pageController");
 
 mongoose
   .connect('mongodb://localhost/blog-test-db')
@@ -22,34 +37,17 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
-app.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
-  res.render('index', {
-    blogs
-  });
-});
-
-app.get('/blogs/:id', async (req, res) => {
-  const blog = await Blog.findById(req.params.id);
-  res.render('blog', {
-    blog
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/blogs', async (req, res) => {
-  await Blog.create(req.body)
-  res.redirect('/');
-});
-
+app.get("/", getAllPosts);
+app.get("/post/:id", getPost);
+app.post("/add", createPost);
+app.put("/post/:id", updatePost);
+app.delete("/post/:id", deletePost);
+app.get("/home", homePage);
+app.get("/about", aboutPage);
+app.get("/add", addPage);
+app.get("/post/edit/:id", editPage);
 
 const port = 3000;
 
